304d0861e31b3f70b21fdcb4751c9933b34e0ca4
Michael Williams <mike@animorphic.com>
2016-07-30 16:52:53 -0400

    Random button generation

--- js/components/App.jsx
+++ js/components/App.jsx
@@ -1,31 +1,36 @@
 import React from 'react';
 import ItemList from './ItemList.jsx';
+import {Button} from 'react-bootstrap';
+
 import {RANDOM_NUM} from '../constants/methods';
 import {CONTAINER_TYPES, CONTAINER_SIZES} from '../constants/containers';
 
 
 export default class App extends React.Component {
 
-	constructor(props) {
-		super(props);
-
-		this.nums = {
-			numContainers: RANDOM_NUM(0, 8),
-			size: RANDOM_NUM(0, CONTAINER_SIZES.length - 1),
-			type: RANDOM_NUM(0, CONTAINER_TYPES.length - 1),
-			chance: RANDOM_NUM(0, 100)
-		};
-
-		
-	}
-
 	render() {
-		let props = this.nums;
+		let buttons = [],
+			numContainers = RANDOM_NUM(0, 8);
+
+		for (let i = 0; i < numContainers; i++) {
+			
+			let size = RANDOM_NUM(0, CONTAINER_SIZES.length - 1),
+				type = RANDOM_NUM(0, CONTAINER_TYPES.length - 1),
+				chance = RANDOM_NUM(0, 100);
+
+			if (CONTAINER_SIZES[size].chance >= chance && CONTAINER_TYPES[type].chance >= chance) {
+				buttons.push(
+					<Button type="button" className="container" data-size={CONTAINER_SIZES[size].type} data-type={CONTAINER_TYPES[type].type}>
+						{CONTAINER_SIZES[size].type + ' ' + CONTAINER_TYPES[type].name}
+					</Button>
+				);
+			}
+		}
 
 		return (
-			<button type="button" className="container" data-size={CONTAINER_SIZES[props.size].type} data-type={CONTAINER_TYPES[props.type].type}>
-				{CONTAINER_SIZES[props.size].type + ' ' + CONTAINER_TYPES[props.type].name}
-			</button>
+			<div>
+				{buttons}
+			</div>
 		);
 	}
 }
